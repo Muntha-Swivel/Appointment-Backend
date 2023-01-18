d5fa0d9b176116e88a1cda126993d51b812b09b9
Muntha-Swivel <munthasir@swivelgroup.com.au>
2023-01-18 17:37:54 +0530

    first commit

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import config from "config";
+import connect from "./utils/connect";
+import log from "./utils/logger";
+import createServer from "./utils/server";
+
+const app = createServer();
+const port = process.env.PORT || config.get<number>("port");
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+app.listen(port, async () => {
+  log.info(`Server started on port ${port}`);
+  await connect();
+});
